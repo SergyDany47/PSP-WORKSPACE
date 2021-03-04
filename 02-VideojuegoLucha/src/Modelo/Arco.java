57e041921b9b58006ef912ef64e8e7a1a65d536d
Sergio <sergiodani47@gmail.com>
2021-03-04 18:59:24 +0100

    Primer Commit

---
+++ 02-VideojuegoLucha/src/Modelo/Arco.java
@@ -0,0 +1,7 @@
+package Modelo;
+
+import Modelo.Arma;
+
+public class Arco extends Arma {
+
+}
---
+++ 02-VideojuegoLucha/src/Modelo/Util.java
@@ -0,0 +1,12 @@
+package Modelo;
+
+import java.util.concurrent.ThreadLocalRandom;
+
+public class Util {
+	
+	public static int generadorNumeros(int rangoA, int rangoB) {
+		int numero = ThreadLocalRandom.current().nextInt(rangoA,rangoB);
+		return numero;
+	}
+
+}
